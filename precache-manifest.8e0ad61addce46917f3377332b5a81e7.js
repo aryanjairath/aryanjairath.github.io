3479b265f1f162d8a05ee0b99dcb4b7ee38ccb17
aryanjairath <aryanjairath7@gmail.com>
2022-08-24 22:33:43 -0400

    Updates

---
+++ precache-manifest.8e0ad61addce46917f3377332b5a81e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc6b72b2ef113b9e1ff1",
+    "url": "/static/css/main.8bc6095d.chunk.css"
+  },
+  {
+    "revision": "cc6b72b2ef113b9e1ff1",
+    "url": "/static/js/main.cc6b72b2.chunk.js"
+  },
+  {
+    "revision": "ddb828c472e469441a37",
+    "url": "/static/js/1.ddb828c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e008cdeb5dfe2e5d4e52cd57e4ed2a1",
+    "url": "/static/media/PaintProgram.4e008cde.png"
+  },
+  {
+    "revision": "82b2a765d12285569b264708d6b4dc72",
+    "url": "/static/media/cv.82b2a765.pdf"
+  },
+  {
+    "revision": "ad881721347591f9ff93a16b9d37e87a",
+    "url": "/static/media/me.jpg.ad881721.jpg"
+  },
+  {
+    "revision": "17bf2df49cca4d4f28fc2184cf40e90a",
+    "url": "/static/media/about-me.17bf2df4.jpg"
+  },
+  {
+    "revision": "184e0f60e1cae795bea2de1c86071717",
+    "url": "/static/media/MusicBox.184e0f60.png"
+  },
+  {
+    "revision": "1a5a02c923d776d81e4dcc6696eafa11",
+    "url": "/static/media/Maze3d.1a5a02c9.png"
+  },
+  {
+    "revision": "d5655c3850d6c789db4d61d1e5e50ef1",
+    "url": "/static/media/bg-texture.d5655c38.jpg"
+  },
+  {
+    "revision": "19b6e6ba11c0e9c5b95861525738aee5",
+    "url": "/index.html"
+  }
+];
